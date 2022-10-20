3eb24e97dd4af0a9f756a733becee4ed6556c254
zarenima <116112101+zarenima@users.noreply.github.com>
2022-10-20 13:17:44 +0330

    v1.1

--- src/ai/UCS.java
+++ src/ai/UCS.java
@@ -1,5 +1,53 @@
 package ai;
 
+import model.Node;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.*;
+
 public class UCS {
+
+    public void search(Node startNode) {
+
+        PriorityQueue<Node> frontier = new PriorityQueue<Node>();
+        Hashtable<String, Boolean> inFrontier = new Hashtable<>();
+        if (startNode.isGoal()) {
+            System.out.println("you win!");
+            printResult(startNode, 0);
+            return;
+        }
+        frontier.add(startNode);
+        inFrontier.put(startNode.hash(), true);
+        while (!frontier.isEmpty()) {
+            Node temp = frontier.poll();
+            inFrontier.remove(temp.hash());
+            ArrayList<Node> children = temp.successor();
+            for (Node child : children) {
+                if (!(inFrontier.containsKey(child.hash()))) {
+                    if (child.isGoal()) {
+                        printResult(child, 0);
+                        System.out.println("you win !!!");
+                        return;
+                    }
+                    frontier.add(child);
+                    inFrontier.put(child.hash(), true);
+                }
+            }
+        }
+        System.out.println("no solution");
+    }
+
+    public void printResult(Node node, int depthCounter) {
+        if (node.getParent() == null) {
+            System.out.println("problem solved at a depth of  : " + depthCounter);
+            return;
+        }
+        System.out.println(node.toString());
+        node.drawState();
+        printResult(node.getParent(), depthCounter + 1);
+    }
+
     
+
 }
